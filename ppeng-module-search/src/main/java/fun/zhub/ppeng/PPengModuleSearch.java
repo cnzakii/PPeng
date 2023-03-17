b51f2ce71843123777235c13e0f9f6c106b89276
zaki <cnzakii@gmail.com>
2023-03-17 20:42:40 +0800

    Create "ppeng-other-api" modules and completed configuration files for all modules

---
+++ ppeng-module-search/src/main/java/fun/zhub/ppeng/PPengModuleSearch.java
@@ -0,0 +1,20 @@
+package fun.zhub.ppeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 主启动类
+ * <p>
+ *
+ * @author Zaki
+ * @version 1.0
+ * @since 2023-03-17
+ **/
+@SpringBootApplication
+public class PPengModuleSearch {
+    public static void main(String[] args) {
+        SpringApplication.run(PPengModuleSearch.class);
+    }
+}
---
+++ ppeng-other-api/src/main/java/fun/zhub/ppeng/PPengOtherApiApplication.java
@@ -0,0 +1,17 @@
+package fun.zhub.ppeng;
+
+/**
+ * <p>
+ *  主启动类
+ * <p>
+ *
+ * @author Zaki
+ * @version 1.0
+ * @since 2023-03-17
+ **/
+
+public class PPengOtherApiApplication {
+    public static void main(String[] args) {
+
+    }
+}
